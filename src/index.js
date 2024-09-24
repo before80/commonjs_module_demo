37aae231069c0c9e968dcab16c6630f69c8ad712
zlongxiang <1251885400@qq.com>
2024-09-24 14:47:50 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const math = require('./math'); // 使用 CommonJS 导入
+
+console.log('Addition:', math.add(1, 2)); // 输出: Addition: 3
+console.log('Subtraction:', math.subtract(5, 3)); // 输出: Subtraction: 2
+console.log('Multiplication:', math.multiply(4, 5)); // 输出: Multiplication: 20
+console.log('Division:', math.divide(10, 2)); // 输出: Division: 5
+
+console.log("1")
